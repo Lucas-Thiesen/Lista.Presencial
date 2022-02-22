e1bd6cd189327b62295fca14022d1ea0fec78dba
Lucas <Lucas@DESKTOP-JUJ0AHE>
2022-02-22 15:26:59 -0300

    Add project files.

---
+++ Exercicio.01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio._01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int largura;
+                int altura;
+                string printe = "";
+
+                Console.WriteLine("Escreva a largura do retangulo: ");
+                largura = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Escreva a altura do retangulo: ");
+                altura = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < altura; i++)
+                {
+                    for (int l = 0; l < largura; l++)
+                    {
+                        printe = printe + "*";
+                    }
+                    printe += "\n";
+                }
+                Console.WriteLine(printe);
+                Console.ReadKey();
+            }
+        }
+    }
+}
